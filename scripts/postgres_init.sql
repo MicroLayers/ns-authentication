9af4c3d49cb405ab826eff903877d9af6a704257
Giacomo Mr. Wolf Furlan <giacomo.furlan@fastwebnet.it>
2018-12-04 07:59:37 +0100

    First commit

---
+++ scripts/postgres_init.sql
@@ -0,0 +1,25 @@
+-- user storage
+CREATE TABLE IF NOT EXISTS %schema%.users (
+  id UUID PRIMARY KEY,
+  username TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP,
+);
+
+-- username / password authentication
+CREATE TABLE IF NOT EXISTS %schema%.authentication_username_password (
+  user_id UUID PRIMARY KEY REFERENCES users.id,
+  password TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP,
+  expiration_date TIMESTAMP
+)
+
+-- bearer token authentication
+CREATE TABLE IF NOT EXISTS %schema%.authentication_tokens (
+  user_id UUID PRIMARY KEY REFERENCES users.id,
+  token TEXT NOT NULL,
+  refresh_token TEXT,
+  created_at TIMESTAMP NOT NULL,
+  expiration_date TIMESTAMP
+)
